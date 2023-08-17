bc9e8af0121f09f1f0fea3dce71412f31d7ffe29
MSWSal <sahansalgado10@gmail.com>
2023-08-17 16:43:05 +0530

    fin

--- CS.py
+++ CS.py
@@ -4,39 +4,13 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 pio.templates.default = "plotly_white"
 
 data = pd.read_csv("train.csv")
 
-print(data.head())
-# print(data.info())
-# print(data.isnull().sum())
 
-# print(data["Credit_Score"].value_counts())
-
-
-# figure = px.box(data,
-# x="Occupation",
-# color="Credit_Score",
-# title="CS based on Occ",
-# color_discrete_map={'Poor':'red',
-#                     'Standard': 'yellow',
-#                     'Good':'green'})
-
-# figure.show()
-
-# figure2 = px.box(data,
-#                  x="Credit_Score",
-#                  y = "Annual_Income",
-#                  color="Credit_Score",
-#                  title="CS based on annual Income",
-#                  color_discrete_map={'Poor':'red',
-#                                      'Standard': 'yellow',
-#                                      'Good':'green'})
-# figure2.update_traces(quartilemethod="exclusive")
-# figure2.show()
 
 data["Credit_Mix"] = data["Credit_Mix"].map({"Standard":1,
                                              "Good":2,
@@ -53,10 +27,23 @@ y = np.array(data[["Credit_Score"]])
 
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.33, random_state=42)
 
-model = RandomForestClassifier()
+model = RandomForestRegressor()
 model.fit(xtrain,ytrain)
 
+print("Credit Score Prediction:")
 
+a = float(input("Annual Income: "))
+b = float(input("Monthly Inhand salary: "))
+c = float(input("Num of Bank accounts: "))
+d= float(input("Num of Credit cards: "))
+e = float(input("Interest rate: "))
+f = float(input("Num of Loans: "))
+g = float(input("Avg num of delays by person: "))
+h = float(input("Num of delayed payments: "))
+i = float(input("cedit Mix(0 for bad, 1 for standard, 3 for good ): "))
+j = float(input("Outstanding debt: "))
+k = float(input("Credit History age: "))
+l = float(input("Monthly balance: "))
 
-
-
+features = np.array([[a,b,c,d,e,f,g,h,i,j,k,l]])
+print("predicted credit score = ", model.predict(features))
