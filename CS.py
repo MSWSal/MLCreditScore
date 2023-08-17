c5328d42c8032b8a4fb663da2640c3c75b84f971
MSWSal <sahansalgado10@gmail.com>
2023-08-17 15:33:02 +0530

    figure 2

--- CS.py
+++ CS.py
@@ -14,14 +14,26 @@ data = pd.read_csv("train.csv")
 # print(data["Credit_Score"].value_counts())
 
 
-figure = px.box(data,
-x="Occupation",
-color="Credit_Score",
-title="CS based on Occ",
-color_discrete_map={'Poor':'red',
-                    'Standard': 'yellow',
-                    'Good':'green'})
+# figure = px.box(data,
+# x="Occupation",
+# color="Credit_Score",
+# title="CS based on Occ",
+# color_discrete_map={'Poor':'red',
+#                     'Standard': 'yellow',
+#                     'Good':'green'})
 
-figure.show()
+# figure.show()
+
+figure2 = px.box(data,
+                 x="Credit_Score",
+                 y = "Annual_Income",
+                 color="Credit_Score",
+                 title="CS based on annual Income",
+                 color_discrete_map={'Poor':'red',
+                                     'Standard': 'yellow',
+                                     'Good':'green'})
+figure2.update_traces(quartilemethod="exclusive")
+figure2.show()
+
 
 
