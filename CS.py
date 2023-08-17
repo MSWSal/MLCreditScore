a79cf1c149c2c5cd7dfdf36ddabaf2d7337aeba0
MSWSal <sahansalgado10@gmail.com>
2023-08-17 16:04:51 +0530

    credit mix map

--- CS.py
+++ CS.py
@@ -3,11 +3,13 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
+from sklearn.model_selection import train_test_split
+
 pio.templates.default = "plotly_white"
 
 data = pd.read_csv("train.csv")
 
-# print(data.head())
+print(data.head())
 # print(data.info())
 # print(data.isnull().sum())
 
@@ -24,16 +26,31 @@ data = pd.read_csv("train.csv")
 
 # figure.show()
 
-figure2 = px.box(data,
-                 x="Credit_Score",
-                 y = "Annual_Income",
-                 color="Credit_Score",
-                 title="CS based on annual Income",
-                 color_discrete_map={'Poor':'red',
-                                     'Standard': 'yellow',
-                                     'Good':'green'})
-figure2.update_traces(quartilemethod="exclusive")
-figure2.show()
+# figure2 = px.box(data,
+#                  x="Credit_Score",
+#                  y = "Annual_Income",
+#                  color="Credit_Score",
+#                  title="CS based on annual Income",
+#                  color_discrete_map={'Poor':'red',
+#                                      'Standard': 'yellow',
+#                                      'Good':'green'})
+# figure2.update_traces(quartilemethod="exclusive")
+# figure2.show()
+
+data["Credit_Mix"] = data["Credit_Mix"].map({"Standard":1,
+                                             "Good":2,
+                                             "Bad":0})
+
+x = np.array(data[["Annual_Income", "Monthly_Inhand_Salary","Num_Bank_Accounts",
+                   "Num_Credit_Card","Inr=terest_Rate","Num_of_Loan",
+                   "Delay_from_due_date","Num_of_Loan",
+                   "Delay_from_due_date","Num_of_Delayed_Payment",
+                   "Credit_Mix","Outstanding_Debt",
+                   "Credit_History_Age","Monthly_Balance"]])
+
+y = np.array(data[["Credit_Score"]])
+
+
 
 
 
